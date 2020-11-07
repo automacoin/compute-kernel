a7b19e07823cb7b7c7a25e62786ef29582e00dc7
Alessio Proietti <alessio.proietti@protonmail.com>
2020-11-08 00:23:28 +0100

    got rid of class, plain javascript

--- script.js
+++ script.js
@@ -1,69 +1,5 @@
-const { GPU } = require('gpu.js');
-
-/** It is needed a ENUM type which could express all possible move, now are three. */
-let direction
-
-(function (direction) {
-    direction[direction["LEFT"] = 0] = "LEFT";
-    direction[direction["STOP"] = 1] = "STOP";
-    direction[direction["RIGHT"] = 2] = "RIGHT";
-})(direction || (direction = {}));
-
-/** The TM's Tape blueprint with the allowable moves */
-class Tape {
-    constructor(blank) {
-        this._tape = [blank];
-        this._head = 0;
-        this.blank = blank;
-    }
-
-    toRight() {
-        if (this._head === this._tape.length - 1) {
-            this._tape.push(this.blank);
-        }
-        this._head++;
-    }
-
-    toLeft() {
-        if (this._head === 0) {
-            this._tape.unshift(this.blank);
-        }
-        this._head--;
-    }
-
-    inscribe(symbol) {
-        this._tape[this._head] = symbol;
-    }
-
-    read() {
-        return this._tape[this._head];
-    }
-
-    print() {
-        return this._tape;
-    }
-}
-
-
-/** Here a definition of a triple which expresses the result of a single step of computation */
-class Result {
-    constructor() {
-        this.control = null;
-        this.write = null;
-        this.dir = null;
-    }
-}
-
-/** The following class represents the instantaneous description of a TM */
-class Description {
-    constructor(control) {
-        this.control = control;
-    }
-}
-
 /** This function define the behaviour of what is a single step of a turing machine computation */
 function step(m, t) {
-    //console.log(m, t);
     r = m.table[m.description.control][t.read()];
 
     t.inscribe(r.write);
@@ -75,10 +11,10 @@ function step(m, t) {
     }
 
     switch (r.dir) {
-        case direction.LEFT:
+        case 0:
             t.toLeft();
             break;
-        case direction.RIGHT:
+        case 2:
             t.toRight();
             break;
     }
@@ -88,19 +24,6 @@ function step(m, t) {
 
 }
 
-/** The actual class which models a complete Turing Machine */
-class TuringMachine {
-    constructor(colors, states, blank, halting, init) {
-        this.table = [];
-        this.colors = colors;
-        this.states = states;
-        this.blank = blank;
-        this.halting = halting;
-        this.description = new Description(init);
-        this.init = init;
-    }
-}
-
 /** This function summarizes the preparatory settings one should set to properly configure a machine */
 function boot(colors, states, blank, number) {
 
@@ -108,15 +31,28 @@ function boot(colors, states, blank, number) {
         return console.log(`{ERROR: number ${number} doesn't represent a TM in the space D(${states},${colors})`);
     }
 
-    let m = new TuringMachine(colors, states, blank, -1, 0);
+    /** The actual object which models a complete Turing Machine */
+    let m = {
+        table: [],
+        colors,
+        states,
+        blank,
+        halting: -1,
+        description: {
+            control: 0
+        },
+        init: 0
+
+    }
+
     const base = colors * ((2 * states) + 1);
-    console.log('Base: ', base)
+    //console.log('Base: ', base)
     let r;
 
     for (let i = 0; i < states; i++) {
         m.table.push([]);
         for (let j = 0; j < colors; j++) {
-            m.table[i].push(new Result());
+            m.table[i].push({});
         }
     }
 
@@ -128,19 +64,18 @@ function boot(colors, states, blank, number) {
             if (r < colors) {
                 m.table[i][j].control = m.halting;
                 m.table[i][j].write = r;
-                m.table[i][j].dir = direction.STOP;
+                m.table[i][j].dir = 1;
             } else {
                 r = r - colors;
                 m.table[i][j].write = (r % colors);
                 r = Math.floor(r / colors);
-                m.table[i][j].dir = ((r % 2) == 0 ? direction.RIGHT : direction.LEFT);
+                m.table[i][j].dir = ((r % 2) == 0 ? 2 : 0);
                 r = Math.floor(r / 2);
                 m.table[i][j].control = (r % states);
             }
         }
     }
 
-    //console.log(m.table)
     return m;
 
 }
@@ -149,33 +84,48 @@ function boot(colors, states, blank, number) {
 function compute() {
 
     let [colors, states, blank, number, runtime] = process.argv.slice(2);
-    let t = new Tape(parseInt(blank));
+    blank = parseInt(blank);
+
+    /** The TM's Tape blueprint with the allowable moves */
+    let t = {
+        _tape: [blank],
+        _head: 0,
+        blank: blank,
+
+
+        toRight: function () {
+            if (this._head === this._tape.length - 1) {
+                this._tape.push(this.blank);
+            }
+            this._head++;
+        },
+
+        toLeft: function () {
+            if (this._head === 0) {
+                this._tape.unshift(this.blank);
+            }
+            this._head--;
+        },
+
+        inscribe: function (symbol) {
+            this._tape[this._head] = symbol;
+        },
+
+        read: function () {
+            return this._tape[this._head];
+        },
+
+        print: function () {
+            return this._tape;
+        },
+    }
+
     let m = boot(colors, states, blank, number);
 
-    do {
-        console.log(t.print(), 'I am here.');
-        runtime = runtime - 1;
-    } while (step(m, t) > 0 && runtime > 0);
+    while (step(m, t) > 0 && runtime --> 0) { }
 
-    return JSON.stringify(t.print());
+    return console.log(t.print());
 }
 
 compute();
 
-/*const gpu = new GPU();
-
-gpu.addFunction(Tape);
-gpu.addFunction(Result);
-gpu.addFunction(Description);
-gpu.addFunction(TuringMachine);
-gpu.addFunction(step);
-gpu.addFunction(boot);
-gpu.addFunction(compute);
-
-const kernel = gpu.createKernel(function() {
-    console.log(compute(2, 2, 0, this.thread.x + 1, 500))
-
-    return 1
-}).setOutput([10]);
-
-kernel();*/
